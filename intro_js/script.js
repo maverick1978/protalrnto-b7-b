7ca2d5f5ecb140c3c0eb86f0c382edfe43314979
Ricardo Bello P <ricardobellop@gmail.com>
2023-05-25 20:11:33 -0500

    inicio protalento

---
+++ intro_js/script.js
@@ -0,0 +1,13 @@
+/*alert("!hola mundo2");
+console.log("Hola desde consola");
+document.write("<h2>hola desde la pagina</h2>");
+*/
+
+let nombre = prompt("Escribe tu nombre: ");
+//document.write(nombre);
+document.write("<h2>!bienvenido  " + nombre + "!</h2>");
+console.log(typeof nombre);
+let edad = 25;
+console.log(typeof edad);
+
+document.write("<h2> mi edad es: " + edad + "</h2>")
